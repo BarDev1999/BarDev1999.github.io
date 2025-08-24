1534d77dbdab6641f67b948a72ea993195a513c0
bar sberro <barsgh@gmail.com>
2025-08-24 17:24:47 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', evt => {
+      if (!form.checkValidity()) {
+        evt.preventDefault();
+        evt.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    });
+  });
+})();
